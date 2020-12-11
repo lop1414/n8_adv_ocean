9c5e2899993ffe8f7d3ab9edbb811dd20cc2ea1e
wutuosen <lop.1414@qq.com>
2020-12-11 17:27:31 +0800

    城市、商圈

---
+++ changelogs/struct/ocean_citys.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50724
+Source Host           : localhost:3306
+Source Database       : n8_adv
+
+Target Server Type    : MYSQL
+Target Server Version : 50724
+File Encoding         : 65001
+
+Date: 2020-12-11 17:26:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for ocean_citys
+-- ----------------------------
+DROP TABLE IF EXISTS `ocean_citys`;
+CREATE TABLE `ocean_citys` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
+  `parent_id` varchar(255) NOT NULL DEFAULT '' COMMENT '父级id',
+  `level` int(11) NOT NULL DEFAULT '0' COMMENT '层级',
+  `created_at` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='巨量地域表';
