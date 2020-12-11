65df65e359a6fb39405dd0eb28febfd4f5896306
wutuosen <lop.1414@qq.com>
2020-12-11 12:08:40 +0800

    filename字符长度

--- changelogs/struct/ocean_videos_20201211.sql
+++ changelogs/struct/ocean_videos_20201211.sql
@@ -2,3 +2,5 @@ ALTER TABLE `ocean_videos`
 DROP INDEX `signature` ,
 ADD INDEX `signature` (`signature`) USING BTREE ;
 
+ALTER TABLE `ocean_videos`
+MODIFY COLUMN `filename`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '素材文件名' AFTER `create_time`;
