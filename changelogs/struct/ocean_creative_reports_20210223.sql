5f693afe5a8b06606733d1faa0592124e561c66a
wutuosen <lop.1414@qq.com>
2021-02-23 14:50:15 +0800

    索引

--- changelogs/struct/ocean_creative_reports_20210223.sql
+++ changelogs/struct/ocean_creative_reports_20210223.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `ocean_creative_reports`
 ADD INDEX `creative_id` (`creative_id`) USING BTREE ;
 
+ALTER TABLE `ocean_creative_reports`
+ADD INDEX `ad_id` (`ad_id`) USING BTREE ,
+ADD INDEX `account_id` (`account_id`) USING BTREE ;
+
