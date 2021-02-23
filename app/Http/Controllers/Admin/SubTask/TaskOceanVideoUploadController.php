48a089a23973e2c7b37a36044abea565797fa6fd
wutuosen <lop.1414@qq.com>
2021-02-23 09:52:43 +0800

    关联视频

--- app/Http/Controllers/Admin/SubTask/TaskOceanVideoUploadController.php
+++ app/Http/Controllers/Admin/SubTask/TaskOceanVideoUploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin\SubTask;
 
+use App\Common\Services\SystemApi\MaterialApiService;
 use App\Models\Task\TaskOceanVideoUploadModel;
 use Illuminate\Http\Request;
 
@@ -15,5 +16,22 @@ class TaskOceanVideoUploadController extends SubTaskOceanController
         $this->model = new TaskOceanVideoUploadModel();
 
         parent::__construct();
+    }
+
+    /**
+     * 列表预处理
+     */
+    public function selectPrepare(){
+        parent::selectPrepare();
+
+        $this->curdService->selectQueryAfter(function(){
+            $videoIds = $this->curdService->responseData['list']->pluck('n8_material_video_id');
+            $materialApiService = new MaterialApiService();
+            $videos = $materialApiService->apiGetVideos($videoIds);
+            $videoMap = array_column($videos, null, 'id');
+            foreach($videoIds = $this->curdService->responseData['list'] as $item){
+                $item->video = $videoMap[$item->n8_material_video_id] ?? null;
+            }
+        });
     }
 }
