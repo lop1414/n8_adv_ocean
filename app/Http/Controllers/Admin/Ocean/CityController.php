a74035921da97ab6f6ee20f3368a46d52a04ddf2
wutuosen <lop.1414@qq.com>
2021-03-17 19:59:27 +0800

    树

--- app/Http/Controllers/Admin/Ocean/CityController.php
+++ app/Http/Controllers/Admin/Ocean/CityController.php
@@ -8,6 +8,18 @@ use Illuminate\Http\Request;
 class CityController extends OceanController
 {
     /**
+     * @var string
+     * 默认排序字段
+     */
+    protected $defaultOrderBy = 'id';
+
+    /**
+     * @var string
+     * 默认排序方式
+     */
+    protected $defaultOrderType = 'asc';
+
+    /**
      * constructor.
      */
     public function __construct()
@@ -28,5 +40,12 @@ class CityController extends OceanController
                 $builder->where('parent_id', $parentId);
             });
         });
+    }
+
+    /**
+     * 树预处理
+     */
+    public function treePrepare(){
+
     }
 }
