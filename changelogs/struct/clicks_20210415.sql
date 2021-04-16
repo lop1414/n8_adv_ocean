5396f7f17b94aa6526a02490bd07433eedd7f7e3
wutuosen <lop.1414@qq.com>
2021-04-16 11:34:15 +0800

    注释

--- changelogs/struct/clicks_20210415.sql
+++ changelogs/struct/clicks_20210415.sql
@@ -3,3 +3,6 @@ ADD COLUMN `click_source`  varchar(50) NOT NULL DEFAULT '' COMMENT '来源' AFTE
 ADD COLUMN `link`  varchar(512) NOT NULL DEFAULT '' COMMENT '落地页原始url' AFTER `caid`,
 ADD COLUMN `extends`  text NOT NULL COMMENT '扩展字段' AFTER `link`;
 
+ALTER TABLE `clicks`
+MODIFY COLUMN `channel_id`  int(11) NOT NULL DEFAULT 0 COMMENT '渠道id' AFTER `request_id`;
+
