72492f939e7e7fb7284c9aebd2e8c004b21a26fd
wutuosen <lop.1414@qq.com>
2021-04-30 16:07:00 +0800

    任务id筛选

--- changelogs/struct/task_ocean_ad_creative_creates_20210430.sql
+++ changelogs/struct/task_ocean_ad_creative_creates_20210430.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `task_ocean_ad_creative_creates`
-ADD COLUMN `ad_id`  varchar(255) NOT NULL DEFAULT '' COMMENT '计划id' AFTER `fail_data`,
+ADD COLUMN `ad_id`  varchar(255) NOT NULL DEFAULT '' COMMENT '计划id' AFTER `fail_data`;
 
