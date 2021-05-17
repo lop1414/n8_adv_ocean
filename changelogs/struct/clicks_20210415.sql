255b8e0522d1f8d2fa5a5bc262cba651d8366c27
wutuosen <lop.1414@qq.com>
2021-05-17 14:59:51 +0800

    link长度

--- changelogs/struct/clicks_20210415.sql
+++ changelogs/struct/clicks_20210415.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `clicks`
 ADD COLUMN `click_source`  varchar(50) NOT NULL DEFAULT '' COMMENT '来源' AFTER `id`,
-ADD COLUMN `link`  varchar(512) NOT NULL DEFAULT '' COMMENT '落地页原始url' AFTER `caid`,
+ADD COLUMN `link`  varchar(1024) NOT NULL DEFAULT '' COMMENT '落地页原始url' AFTER `caid`,
 ADD COLUMN `extends`  text NOT NULL COMMENT '扩展字段' AFTER `link`;
 
 ALTER TABLE `clicks`
