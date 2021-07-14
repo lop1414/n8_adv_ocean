01d7d92f96c66035117825c70a4cf60c0f9a09f9
wutuosen <lop.1414@qq.com>
2021-07-14 17:16:53 +0800

    激活

--- app/Services/AdvConvertCallbackService.php
+++ app/Services/AdvConvertCallbackService.php
@@ -100,6 +100,7 @@ class AdvConvertCallbackService extends ConvertCallbackService
      */
     public function getEventTypeMap(){
         return [
+            ConvertTypeEnum::ACTIVATION => 0,
             ConvertTypeEnum::ADD_DESKTOP => 1,
             ConvertTypeEnum::PAY => 2,
         ];
