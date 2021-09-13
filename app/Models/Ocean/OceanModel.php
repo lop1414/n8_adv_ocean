c4ae9e64686b669c80b85502d3c30d5c9b1a589f
wutuosen <lop.1414@qq.com>
2021-09-13 11:43:30 +0800

    数据授权

--- app/Models/Ocean/OceanModel.php
+++ app/Models/Ocean/OceanModel.php
@@ -9,18 +9,37 @@ class OceanModel extends BaseModel
 {
     /**
      * @param $query
+     * @return mixed
      * 数据授权
      */
     public function scopeWithPermission($query){
         $adminUserInfo = Functions::getGlobalData('admin_user_info');
         $table = $this->getTable();
-//        if(!$adminUserInfo['is_admin']){
-            $query->whereRaw("
+        $query->whereRaw("
+            {$table}.account_id IN (
+                SELECT account_id FROM ocean_accounts
+                    WHERE admin_id = {$adminUserInfo['admin_user']['id']}
+            )
+        ");
+        return $query;
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     * 管理员数据授权
+     */
+    public function scopeWithAdminPermission($query){
+        $adminUserInfo = Functions::getGlobalData('admin_user_info');
+        $table = $this->getTable();
+        if(!$adminUserInfo['is_admin']){
+        $query->whereRaw("
                 {$table}.account_id IN (
                     SELECT account_id FROM ocean_accounts
                         WHERE admin_id = {$adminUserInfo['admin_user']['id']}
                 )
             ");
-//        }
+        }
+        return $query;
     }
 }
