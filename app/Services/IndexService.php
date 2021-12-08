5b497f9726eb30536131299a79f3c388080781b1
wutuosen <lop.1414@qq.com>
2021-12-08 11:29:16 +0800

    创意监控

--- app/Services/IndexService.php
+++ app/Services/IndexService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Common\Services\BaseService;
 use App\Common\Services\SystemApi\CenterApiService;
 use App\Enums\Ocean\OceanAdStatusEnum;
+use App\Enums\Ocean\OceanCreativeStatusEnum;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,44 +30,6 @@ class IndexService extends BaseService
         $deleteStatus = OceanAdStatusEnum::AD_STATUS_DELETE;
 
         $result = [];
-
-        $sql = "
-            SELECT
-                ocean_accounts.admin_id,
-                count(DISTINCT ocean_ads.id) run_ads
-            FROM
-                ocean_ads
-            LEFT JOIN ocean_accounts ON ocean_ads.account_id = ocean_accounts.account_id
-            WHERE
-                ocean_ads.`status` = '{$okStatus}'
-                AND ocean_accounts.admin_id > 0
-                AND (
-                    ocean_accounts.account_id IN (
-                        SELECT
-                            account_id
-                        FROM
-                            ocean_account_funds
-                        WHERE
-                            valid_balance > 0
-                    )
-                    OR ocean_accounts.account_id IN (
-                        SELECT
-                            account_id
-                        FROM
-                            ocean_account_reports
-                        WHERE
-                            stat_datetime BETWEEN '{$date} 00:00:00'
-                        AND '{$date} 23:59:59'
-                        AND cost > 0
-                    )
-                )
-            GROUP BY
-                ocean_accounts.admin_id
-            ORDER BY
-                run_ads DESC
-        ";
-
-        $result['run_ads'] = array_map('get_object_vars', DB::select($sql));
 
         $sql = "
             SELECT
@@ -107,6 +70,83 @@ class IndexService extends BaseService
         $sql = "
             SELECT
                 ocean_accounts.admin_id,
+                count(DISTINCT ocean_ads.id) run_ads
+            FROM
+                ocean_ads
+            LEFT JOIN ocean_accounts ON ocean_ads.account_id = ocean_accounts.account_id
+            WHERE
+                ocean_ads.`status` = '{$okStatus}'
+                AND ocean_accounts.admin_id > 0
+                AND (
+                    ocean_accounts.account_id IN (
+                        SELECT
+                            account_id
+                        FROM
+                            ocean_account_funds
+                        WHERE
+                            valid_balance > 0
+                    )
+                    OR ocean_accounts.account_id IN (
+                        SELECT
+                            account_id
+                        FROM
+                            ocean_account_reports
+                        WHERE
+                            stat_datetime BETWEEN '{$date} 00:00:00'
+                        AND '{$date} 23:59:59'
+                        AND cost > 0
+                    )
+                )
+            GROUP BY
+                ocean_accounts.admin_id
+            ORDER BY
+                run_ads DESC
+        ";
+
+        $result['run_ads'] = array_map('get_object_vars', DB::select($sql));
+
+        $creativeOkStatus = OceanCreativeStatusEnum::CREATIVE_STATUS_DELIVERY_OK;
+        $sql = "
+            SELECT
+                ocean_accounts.admin_id,
+                count(DISTINCT ocean_creatives.id) run_creatives
+            FROM
+                ocean_creatives
+            LEFT JOIN ocean_accounts ON ocean_creatives.account_id = ocean_accounts.account_id
+            WHERE
+                ocean_creatives.`status` = '{$creativeOkStatus}'
+                AND ocean_accounts.admin_id > 0
+                AND (
+                    ocean_accounts.account_id IN (
+                        SELECT
+                            account_id
+                        FROM
+                            ocean_account_funds
+                        WHERE
+                            valid_balance > 0
+                    )
+                    OR ocean_accounts.account_id IN (
+                        SELECT
+                            account_id
+                        FROM
+                            ocean_account_reports
+                        WHERE
+                            stat_datetime BETWEEN '{$date} 00:00:00'
+                        AND '{$date} 23:59:59'
+                        AND cost > 0
+                    )
+                )
+            GROUP BY
+                ocean_accounts.admin_id
+            ORDER BY
+                run_creatives DESC
+        ";
+
+        $result['run_creatives'] = array_map('get_object_vars', DB::select($sql));
+
+        $sql = "
+            SELECT
+                ocean_accounts.admin_id,
                 COUNT(*) created_ads
             FROM
                 ocean_ads
@@ -121,6 +161,25 @@ class IndexService extends BaseService
                 created_ads DESC
         ";
         $result['created_ads'] = array_map('get_object_vars', DB::select($sql));
+
+        $creativeDeleteStatus = OceanCreativeStatusEnum::CREATIVE_STATUS_DELETE;
+        $sql = "
+            SELECT
+                ocean_accounts.admin_id,
+                COUNT(*) created_creatives
+            FROM
+                ocean_creatives
+            LEFT JOIN ocean_accounts ON ocean_creatives.account_id = ocean_accounts.account_id
+            WHERE
+                ocean_creatives.creative_create_time BETWEEN '{$date} 00:00:00'
+            AND '{$date} 23:59:59'
+            /*AND ocean_creatives.`status` != '{$creativeDeleteStatus}'*/
+            GROUP BY
+                ocean_accounts.admin_id
+            ORDER BY
+                created_creatives DESC
+        ";
+        $result['created_creatives'] = array_map('get_object_vars', DB::select($sql));
 
         $sql = "
             SELECT
@@ -170,6 +229,31 @@ class IndexService extends BaseService
         ";
         $result['has_cost_ads'] = array_map('get_object_vars', DB::select($sql));
 
+        $sql = "
+            SELECT
+                ocean_accounts.admin_id,
+                COUNT(*) has_cost_creatives
+            FROM
+                (
+                    SELECT
+                        account_id,
+                        creative_id
+                    FROM
+                        ocean_creative_reports
+                    WHERE
+                        stat_datetime BETWEEN '{$date} 00:00:00'
+                    AND '{$date} 23:59:59'
+                    AND cost > 0
+                    GROUP BY
+                        account_id,
+                        creative_id
+                ) report
+            LEFT JOIN ocean_accounts ON report.account_id = ocean_accounts.account_id
+            GROUP BY
+                ocean_accounts.admin_id
+        ";
+        $result['has_cost_creatives'] = array_map('get_object_vars', DB::select($sql));
+
         // 管理员映射
         $centerApiService = new CenterApiService();
         $adminUsers = $centerApiService->apiGetAdminUsers();
@@ -177,11 +261,14 @@ class IndexService extends BaseService
 
         // 默认
         $default = [
-            'run_ads' => 0,
             'run_accounts' => 0,
+            'run_ads' => 0,
+            'run_creatives' => 0,
             'created_ads' => 0,
+            'created_creatives' => 0,
             'has_cost_accounts' => 0,
             'has_cost_ads' => 0,
+            'has_cost_creatives' => 0,
         ];
 
         // 映射
