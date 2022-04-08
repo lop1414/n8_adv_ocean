88c717ed2fbd46e516669ac619aed36204e53785
wutuosen <lop.1414@qq.com>
2022-04-08 17:57:29 +0800

    并发获取账户日流水

--- app/Sdks/OceanEngine/Traits/Account.php
+++ app/Sdks/OceanEngine/Traits/Account.php
@@ -115,4 +115,20 @@ trait Account
 
         return $this->authRequest($url, $param, 'GET');
     }
+
+    /**
+     * @param array $accountIds
+     * @param $accessToken
+     * @param array $filtering
+     * @param int $page
+     * @param int $pageSize
+     * @param array $param
+     * @return mixed
+     * 并发获取账户日流水
+     */
+    public function multiGetAccountFundDailyStat(array $accountIds, $accessToken, $filtering = [], $page = 1, $pageSize = 10, $param = []){
+        $url = $this->getUrl('2/advertiser/fund/daily_stat/');
+
+        return $this->multiGetPageList($url, $accountIds, $accessToken, $filtering, $page, $pageSize, $param);
+    }
 }
--- app/Services/Ocean/OceanAccountFundDailyStatService.php
+++ app/Services/Ocean/OceanAccountFundDailyStatService.php
@@ -18,6 +18,20 @@ class OceanAccountFundDailyStatService extends OceanService
     }
 
     /**
+     * @param $accountIds
+     * @param $accessToken
+     * @param $filtering
+     * @param $page
+     * @param $pageSize
+     * @param array $param
+     * @return mixed|void
+     * sdk批量获取列表
+     */
+    public function sdkMultiGetList($accountIds, $accessToken, $filtering, $page, $pageSize, $param = []){
+        return $this->sdk->multiGetAccountFundDailyStat($accountIds, $accessToken, $filtering, $page, $pageSize, $param);
+    }
+
+    /**
      * @param $option
      * @return bool
      * @throws CustomException
@@ -36,6 +50,11 @@ class OceanAccountFundDailyStatService extends OceanService
             $accountIds = $this->getHasHistoryCostAccount($accountIds);
         }
 
+        // 并发分片大小
+        if(!empty($option['multi_chunk_size'])){
+            $this->sdk->setMultiChunkSize(2);
+        }
+
         $builder = new OceanAccountModel();
         $builder = $builder->enable();
 
@@ -51,43 +70,58 @@ class OceanAccountFundDailyStatService extends OceanService
         }
         $oceanAccounts = $builder->get();
 
+
+        // 获取子账户组
+        $accountGroup = $this->getSubAccountGroup($accountIds);
+
         $dateRange = Functions::getDateRange($option['date']);
         $dateList = Functions::getDateListByRange($dateRange);
 
         foreach($dateList as $date){
-            $data = [];
-            foreach($oceanAccounts as $oceanAccount){
-                $this->setAppId($oceanAccount->app_id);
-                $this->setAccountId($oceanAccount->account_id);
-                $this->setAccessToken();
+            $param = [
+                'start_date' => $date,
+                'end_date' => $date,
+            ];
 
-                $ret = $this->sdk->getAccountFundDailyStat($oceanAccount->account_id, $date, $date);
+            $pageSize = 200;
+            foreach($accountGroup as $g){
+                $items = $this->multiGetPageList($g, [], $pageSize, $param);
 
-                $item = current($ret['list']);
+                Functions::consoleDump('count:'. count($items));
 
-                if($item['cost'] <= 0){
-                    continue;
+                $cost = 0;
+
+                // 保存
+                $data = [];
+                foreach($items as $item) {
+                    $cost += $item['cost'];
+
+                    if($item['cost'] <= 0){
+                        continue;
+                    }
+
+                    $data[] = [
+                        'account_id' => $item['advertiser_id'],
+                        'stat_datetime' => "{$item['date']} 00:00:00",
+                        'balance' => $item['balance'] * 100,
+                        'cash_cost' => $item['cash_cost'] * 100,
+                        'frozen' => $item['frozen'] * 100,
+                        'income' => $item['income'] * 100,
+                        'reward_cost' => $item['reward_cost'] * 100,
+                        'shared_wallet_cost' => $item['shared_wallet_cost'] * 100,
+                        'transfer_in' => $item['transfer_in'] * 100,
+                        'transfer_out' => $item['transfer_out'] * 100,
+                        'extends' => json_encode($item),
+                        'created_at' => $datetime,
+                        'updated_at' => $datetime,
+                    ];
                 }
 
-                $data[] = [
-                    'account_id' => $oceanAccount->account_id,
-                    'stat_datetime' => "{$item['date']} 00:00:00",
-                    'balance' => $item['balance'] * 100,
-                    'cash_cost' => $item['cash_cost'] * 100,
-                    'frozen' => $item['frozen'] * 100,
-                    'income' => $item['income'] * 100,
-                    'reward_cost' => $item['reward_cost'] * 100,
-                    'shared_wallet_cost' => $item['shared_wallet_cost'] * 100,
-                    'transfer_in' => $item['transfer_in'] * 100,
-                    'transfer_out' => $item['transfer_out'] * 100,
-                    'extends' => json_encode($item),
-                    'created_at' => $datetime,
-                    'updated_at' => $datetime,
-                ];
-                sleep(1);
-            }
+                // 批量保存
+                $this->batchSave($data);
 
-            $this->batchSave($data);
+                Functions::consoleDump('cost:'. $cost);
+            }
         }
 
         return true;
